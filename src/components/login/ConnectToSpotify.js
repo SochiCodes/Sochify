a147418f36ce7bdb9aacb3551843312e12a03636
SochiCodes <you@example.com>
2023-03-12 04:44:15 +0100

    I also created the login page

--- src/components/login/ConnectToSpotify.js
+++ src/components/login/ConnectToSpotify.js
@@ -7,7 +7,7 @@ export default function ConnectToSpotify() {
   return (
     <div className='connect-2-spotify'>
         <img src="/img/logo.png" alt="" />
-        <a href={loginEndPoint} className="login"> Connect to Spotify</a>
+        <a href={loginEndPoint} className="login"> Login With Spotify</a>
     </div>
   )
 }
--- src/spotify.js
+++ src/spotify.js
@@ -14,18 +14,7 @@ const scopes = [
     "user-read-playback-position",
     "user-top-read",
 ];
-//getting the access Token
-// export  const getTokenFromUrl = () =>{
-//     return window.location.hash
-//     .substring(1)
-//     .split('&')
-//     .reduce((initial, item) => {
-//         let parts = item.split('=');
-//         initial[parts[0]] = decodeURIComponent(parts[1])
 
-//         return initial
-//     }, {});
-// }
 
 export  const loginEndPoint = `${authEndPoint}client_id=${clientID}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
 
